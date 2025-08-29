0aac3aee83ff9ae263778bfad1644dfe66833b55
Maximilian <scientificresearch309@gmail.com>
2025-08-29 23:07:26 +0200

    converting a normal variable to a state varibale

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+
+// let score = 0; Normal variable => we will change it from a normal variable to a state variable below!
+
 function App() {
+  const [count, setCount] = useState(0); // State variable
+  const [score, setScore] = useState(0); // change the Normal variable to a state variable!
+
+  const handleStateVariable = () => {
+    setCount(count + 1);
+  };
+
+  const handleNormalVariable = () => {
+    // console.log(score);
+    // score = score + 1;
+    // score++;
+    // score += 1;
+    setScore(score + 1);
+  };
+
   return (
     <div className="App">
       <h1>useState-eventHandler</h1>
+      <button onClick={handleStateVariable}>
+        State Variable Button {count}
+      </button>
+
+      <button onClick={handleNormalVariable}>
+        Normal Variable Button {score}
+      </button>
     </div>
   );
 }
