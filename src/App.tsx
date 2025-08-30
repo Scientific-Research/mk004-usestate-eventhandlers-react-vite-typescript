14327a9ee553a5c330ab599bdb6614c96b41d451
Maximilian <scientificresearch309@gmail.com>
2025-08-30 23:27:20 +0200

    when score is more than 5 then color would be red, otherwise NO

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" style={score > 5 ? { backgroundColor: 'red' } : {}}>
       <h1>useState-eventHandler</h1>
       <hr />
 
