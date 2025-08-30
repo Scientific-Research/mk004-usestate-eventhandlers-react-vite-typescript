fb7de2ea6770f8a3ad5bd391fb058b0c30bdf066
Maximilian <scientificresearch309@gmail.com>
2025-08-30 13:35:54 +0200

    new state varibale as color was defined

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import { useState } from 'react';
 
 // let score = 0; Normal variable => we will change it from a normal variable to a state variable below!
 
+const _colors = ['green', 'blue', 'yellow'];
+
 function App() {
   const [count, setCount] = useState(0); // State variable
   const [score, setScore] = useState(0); // change the Normal variable to a state variable!
+  const [colors, setColors] = useState(_colors);
+
+  // string, boolean, number => stack
+  // arrays, objects => heap
+  // React state is immutable
 
   const handleStateVariable = () => {
     setCount(count + 1);
